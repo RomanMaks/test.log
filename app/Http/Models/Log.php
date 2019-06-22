edd71e46dfad90710e39adb2aeab766ab8e69c1d
Roman Maksimov <maksimov.ro.al@gmail.com>
2019-06-22 19:30:12 +0300

    Добавил модель Log и поправил миграцию

---
+++ app/Http/Models/Log.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Log
+ * @package App\Http\Models
+ *
+ * @property integer $id
+ * @property string $ip
+ * @property Carbon $date
+ * @property string|null $url
+ * @property string $os
+ * @property string|null $architecture
+ * @property string $browser
+ */
+class Log extends Model
+{
+    protected $table = 'logs';
+
+    /**
+     * Атрибуты которые можно присвоить массово
+     *
+     * @var array $fillable
+     */
+    protected $fillable = [
+        'date',
+        'url',
+        'os',
+        'architecture',
+        'browser'
+    ];
+}
